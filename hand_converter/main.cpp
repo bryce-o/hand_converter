72c8f40fae1df5c5b9c38f09377fddb4e3890e15
Bryce Owens <bryceowens@Bryces-MacBook-Pro.local>
2018-01-01 14:57:39 -0500

    Initial Commit

---
+++ hand_converter/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  hand_converter
+//
+//  Created by Bryce Owens on 1/1/18.
+//  Copyright © 2018 Bryce Owens. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
